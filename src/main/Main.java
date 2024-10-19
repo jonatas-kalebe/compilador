2978a3d36213e509c4d4f6aa6e2c6d07e36812c7
jonatas <jonatas.mfreitas@detran.go.gov.br>
2024-10-19 16:26:57 -0300

    v3.1 teoricamente agora lê arquivos

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,147 +5,75 @@ import estruturas.MainStatements;
 import util.RegexUtil;
 import util.StatementsParser;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class Main {
-    private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
     public static void main(String[] args) {
+        if (args.length != 2) {
+            System.err.println("Uso: java BoolCompiler <arquivo_entrada>.bool <arquivo_saida>.boolc");
+            System.exit(1);
+        }
 
-        String code = """
-                class Animal
-                 vars name, age
-                
-                 method speak()
-                 vars n
-                 begin
-                 n = self.name
-                 io.print(n)
-                 return 0
-                 end-method
-                
-                 method grow()
-                 vars a, one
-                 begin
-                 a = self.age
-                 one = 1
-                 a = a + one
-                 self.age = a
-                 return self.age
-                 end-method
-                end-class
-                
-                class Dog
-                 vars breed
-                
-                 method bark()
-                 vars n
-                 begin
-                 n = 100
-                 io.print(n)
-                 return 0
-                 end-method
-                end-class
-                
-                class Cat
-                 vars color
-                
-                 method meow()
-                 vars n
-                 begin
-                 n = 200
-                 io.print(n)
-                 return 0
-                 end-method
-                end-class
-                
-                main()
-                vars a, d, c, num, res, temp
-                begin
-                 a = new Animal
-                 d = new Dog
-                 c = new Cat
-                
-                 a.name = 10
-                 a.age = 5
-                
-                 d._prototype = a
-                 c._prototype = a
-                
-                 num = 1
-                
-                 if num eq 1 then
-                  d.breed = 20
-                  d.name = 30
-                  d.age = 3
-                  res = d.grow()
-                  io.print(res)
-                 else
-                  c.color = 40
-                  c.name = 50
-                  c.age = 2
-                  res = c.grow()
-                  io.print(res)
-                 end-if
-                
-                 a = new Animal
-                
-                 num = num + 2
-                 num = num * 3
-                 num = num - 4
-                 num = num / 5
-                
-                 res = d.grow()
-                 io.print(res)
-                
-                 d.speak()
-                end
-            """;
+        String inputFileName = args[0];
+        String outputFileName = args[1];
 
+        String code;
+        try {
+            code = Files.readString(Path.of(inputFileName));
+        } catch (IOException e) {
+            System.err.println("Erro ao ler o arquivo de entrada: " + e.getMessage());
+            System.exit(1);
+            return;
+        }
 
+        String finalCode = compileBoolCode(code);
 
-        String main = RegexUtil.extractMain(code).getFirst();
+        try {
+            Files.writeString(Path.of(outputFileName), finalCode);
+        } catch (IOException e) {
+            System.err.println("Erro ao escrever o arquivo de saída: " + e.getMessage());
+            System.exit(1);
+        }
+    }
 
-        List<String> mainVars = RegexUtil.extractVars(main);
+    private static String compileBoolCode(String code) {
+        String mainBlockString = RegexUtil.extractMain(code).getFirst();
 
-        List<MainStatements> mainStatements = StatementsParser.processEachLineMainStatement(main);
+        List<String> mainVars = RegexUtil.extractVars(mainBlockString);
+        List<MainStatements> mainStatements = StatementsParser.processEachLineMainStatement(mainBlockString);
 
-        MainBlock mainBlock = new MainBlock(mainStatements,mainVars);
+        MainBlock mainBlock = new MainBlock(mainStatements, mainVars);
 
         List<ClassesBlock> classesBlockList = new ArrayList<>();
-
-
         List<String> classes = RegexUtil.extractClasses(code);
 
         for (String classe : classes) {
-
             String className = RegexUtil.findName(classe);
-
             List<String> vars = RegexUtil.extractVars(classe);
 
             List<String> methods = RegexUtil.extractMethods(classe);
-            List<MethodDef> metodos = new ArrayList<>();
+            List<MethodDef> methodsDef = new ArrayList<>();
 
             for (String method : methods) {
                 String methodName = RegexUtil.findMethodName(method);
                 List<String> methodVars = RegexUtil.extractVars(method);
-                List<String> methodParams=RegexUtil.extractMethodParams(method);
+                List<String> methodParams = RegexUtil.extractMethodParams(method);
                 String methodBodyString = RegexUtil.extractMethodBody(method);
+
                 MethodBody methodBody = StatementsParser.processEachLineMethodStatement(methodBodyString);
-                MethodHeader methodHeader=new MethodHeader(methodName,methodParams);
-                metodos.add( new MethodDef(methodBody,methodHeader,methodVars));
+                MethodHeader methodHeader = new MethodHeader(methodName, methodParams);
+                methodsDef.add(new MethodDef(methodBody, methodHeader, methodVars));
             }
-            ClassesBlock classesBlock=new ClassesBlock(className,vars,metodos);
+            ClassesBlock classesBlock = new ClassesBlock(className, vars, methodsDef);
             classesBlockList.add(classesBlock);
         }
 
-        Program program = new Program(mainBlock,classesBlockList);
+        Program program = new Program(mainBlock, classesBlockList);
 
-        String finalCode = program.compileCode();
-
-        LOGGER.log(Level.INFO,finalCode);
-
+        return program.compileCode();
     }
 }
